2608d156a9ff35aa75a9ad4fcd532449c0f8ea01
Ashish Bishnoi <bishnoi11011@gmail.com>
2025-01-13 13:28:19 +0530

    second commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-      apiToken: "<vercel-api-token>",
-      appFilter: "your-app-name-on-vercel",
-      teamFilter: "team_djjmnnN9oxAnOzT2cZy400tf",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
-  },
+  // "vercel-deploy": {
+  //   enabled: true,
+  //   config: {
+  //     deployHook:
+  //       "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+  //     apiToken: "<vercel-api-token>",
+  //     appFilter: "your-app-name-on-vercel",
+  //     teamFilter: "team_djjmnnN9oxAnOzT2cZy400tf",
+  //     roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+  //   },
+  // },
 
   // ...
   upload: {
--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,12 +1,23 @@
 module.exports = ({ env }) => ({
+    // "vercel-deploy": {
+    //   enabled: true,
+    //   config: {
+    //     deployHook:
+    //       "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+    //     apiToken: "<vercel-api-token>",
+    //     appFilter: "your-app-name-on-vercel",
+    //     teamFilter: "team_djjmnnN9oxAnOzT2cZy400tf",
+    //     roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    //   },
+    // },
     // ...
     upload: {
         config: {
             provider: "aws-s3",
             providerOptions: {
                 credentials: {
-                    accessKeyId: "AKIAVWABJYHYKCOGIIOA",
-                    secretAccessKey: "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",
+                    accessKeyId: process.env.S3_ACCESS_KEY,
+                    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
                 },
                 region: "ap-south-1",
                 baseUrl: `https://av-blog.s3.ap-south-1.amazonaws.com`, // This line sets the custom url format
